6e2bf4b5350bc495edfaaef7554eab9b3c56c5c1
lonewolf235 <gagantomar235@gmail.com>
2023-06-14 02:31:23 +0530

    LinkedList Reversal

---
+++ llreversal.java
@@ -0,0 +1,32 @@
+public class llreversal {
+
+  //* Definition for singly-linked list.
+  public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+      this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+//This is a iterative method
+  public ListNode reverseList(ListNode head) {
+    ListNode temp = head;
+    ListNode prev = null;
+    while (temp != null) {
+      ListNode n = temp.next;
+      temp.next = prev;
+      prev = temp;
+      temp = n;
+    }
+    return prev;
+  }
+}
