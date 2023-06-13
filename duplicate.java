066480d8396558410e7666e71c61eb5dc798414e
lonewolf235 <gagantomar235@gmail.com>
2023-06-14 02:06:41 +0530

    Duplicate number Array Solution

---
+++ duplicate.java
@@ -0,0 +1,17 @@
+public class duplicate {
+    public int findDuplicate(int[] nums) {
+        int slow=nums[0];
+        int fast=nums[0];
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow!=fast);
+        fast=nums[0];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
+    }
+}
+//Knew the hashmap approach,but found this beautiful O(1)space approach.
