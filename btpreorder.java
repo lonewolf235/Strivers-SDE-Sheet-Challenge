3edf23633def90f0e645c304d1be638ce316a0f4
lonewolf235 <gagantomar235@gmail.com>
2023-06-14 01:25:35 +0530

    added binary tree preorder traversal

---
+++ btpreorder.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class btpreorder {
+ // Definition for a binary tree node.
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    List<Integer>ls=new ArrayList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        // List<Integer> ls=new ArrayList<>();
+        // if(root==null)
+        // return ls;
+        // Stack<TreeNode> st=new Stack<>();
+        // st.push(root);
+        // while(!st.isEmpty()){
+        //    root=st.pop();
+        //     ls.add(root.val);
+        //     if(root.right!=null)
+        //     st.push(root.right);
+        //     if(root.left!=null)
+        //     st.push(root.left);
+        // }
+        // return ls;
+        preorder(root);
+        return ls;
+    }
+    public void preorder(TreeNode root){
+        if(root==null)
+        return;
+        ls.add(root.val);
+        preorder(root.left);
+        preorder(root.right);
+    }
+} {
+    
+}
